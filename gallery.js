3a50dc4c67aa23e446a4552c7535ebd7abb3c617
Wyatt987 <52wbedor@frhsd.com>
2025-11-12 11:14:26 -0500

    Implement next and previous button functionality

--- gallery.js
+++ gallery.js
@@ -17,6 +17,12 @@ $(document).ready(() => {
   // Select the "Previous Photo" button and add a click event to call showPrevPhoto
 
   // Call fetchJSON() to load the initial set of images
+    $('#nextPhoto').click(() => {
+    showNextPhoto();
+  })
+  $('#prevPhoto').click(() => {
+    showPrevPhoto();
+  })
   fetchJSON()
 })
 
@@ -55,13 +61,24 @@ function fetchJSON() {
 function showNextPhoto () {
   // Increment mCurrentIndex and call swapPhoto()
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
-  
+    startTimer();
+  mCurrentIndex++;
+  if (mCurrentIndex >= mImages.length) {
+    mCurrentIndex = 0
+  }
+  swapPhoto();
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto () {
   // Decrement mCurrentIndex and call swapPhoto()
   // Ensure it loops to the end if mCurrentIndex is less than 0
+    startTimer();
+  mCurrentIndex--;
+  if (mCurrentIndex < 0) {
+    mCurrentIndex = mImages.length - 1
+  }
+  swapPhoto();
 }
 
 // Starter code for the timer function
