a85a11f01c55eca15c4f212a0eba3a4866dc7f11
Wyatt987 <52wbedor@frhsd.com>
2025-11-12 11:05:11 -0500

    Implement AJAX request to load JSON data

--- gallery.js
+++ gallery.js
@@ -21,11 +21,21 @@ $(document).ready(() => {
 })
 
 // Function to fetch JSON data and store it in mImages
-function fetchJSON () {
   // Use $.ajax here to request the JSON data from mUrl
   // On success, parse the JSON and push each image object into mImages array
   // After JSON is loaded, call swapPhoto() to display the first image
-  
+function fetchJSON() {
+  $.ajax({
+    url: mUrl,
+    dataType: 'json',
+    success: function (data) {
+      mImages = data.images;
+      swapPhoto()
+    },
+    error: function (xhr, status, error) {
+      console.error("Failed to load")
+    }
+  })
 }
 
 // Function to swap and display the next photo in the slideshow
